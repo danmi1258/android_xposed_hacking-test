3fe609d0d0514dd3928d7f042a2614077db123a1
M1cha <sigmaepsilon92@gmail.com>
2014-07-17 21:09:34 +0200

    add typical LicenseApp test crackmodule can successfully bypass this already

--- CrackMe/src/com/github/crackme/MainActivity.java
+++ CrackMe/src/com/github/crackme/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.crackme;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,13 +17,45 @@ public class MainActivity extends Activity {
 		mTextView = (TextView) findViewById(R.id.textView1);
 	}
 	
+	private void runTest(StringBuilder sb, String fn) {
+		sb.append(fn+": ");
+
+		try {
+			boolean rc = (boolean)this.getClass().getMethod(fn).invoke(this);
+			if(rc)
+					sb.append("pass");
+			else	sb.append("fail");
+		} catch (Exception e) {
+			e.printStackTrace();
+			sb.append("EXCEPTION:"+e.getLocalizedMessage());
+		}
+
+		sb.append("\n");
+	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("Tests\n==========\n\n");
+
+		runTest(sb, "checkProApp");
+
+		mTextView.setText(sb+getApplicationContext().getPackageName());
+	}
+
+	public boolean checkProApp() {
+		PackageManager pm = getPackageManager();
+		String pkgMain = getApplicationContext().getPackageName();
+		String pkgPro = pkgMain+".pro";
 		
-		mTextView.setText(sb);
+		try {
+			pm.getPackageInfo(pkgPro, PackageManager.GET_ACTIVITIES);
+		} catch (NameNotFoundException e) {
+			return false;
+		}
+		
+		return pm.checkSignatures(pkgMain, pkgPro)==PackageManager.SIGNATURE_MATCH;
 	}
 }
--- CrackModule/src/com/github/xposed/crackmodule/XposedMain.java
+++ CrackModule/src/com/github/xposed/crackmodule/XposedMain.java
@@ -1,5 +1,50 @@
 package com.github.xposed.crackmodule;
 
-public class XposedMain {
+import android.content.pm.PackageManager;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class XposedMain implements IXposedHookLoadPackage {
+
+	@Override
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+		if (!lpparam.packageName.equals("com.github.crackme"))
+			return;
+
+		crackProApp(lpparam);
+	}
+
+	private void crackProApp(LoadPackageParam lpparam) {
+		findAndHookMethod("android.app.ApplicationPackageManager",
+				lpparam.classLoader, "getPackageInfo", String.class, int.class,
+				new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param)
+							throws Throwable {
+						String packageName = (String) param.args[0];
+
+						// pretend the app to exist
+						if (packageName.equals("com.github.crackme.pro"))
+							param.args[0] = "com.github.crackme";
+					}
+				});
+
+		findAndHookMethod("android.app.ApplicationPackageManager",
+				lpparam.classLoader, "checkSignatures", String.class,
+				String.class, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param)
+							throws Throwable {
+						String pkg1 = (String) param.args[0];
+						String pkg2 = (String) param.args[1];
+
+						// pretend matching signatures
+						if (pkg2.equals(pkg1+".pro"))
+							param.setResult(PackageManager.SIGNATURE_MATCH);
+					}
+				});
+	}
 
 }
